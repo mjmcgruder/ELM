b812f7ec57b1ef2b1eecfce82667ec860d20fea0
miles <milesjmcgruder@gmail.com>
2024-02-24 13:47:52 -0500

    intermediate - working on reduction
    More confident with this CPU-GPU reduction setup. It looks like this is
    relatively easy to handle with simple use of templates. Unsupported
    operations should be automatically rejected at compile time and CPU/GPU
    should perform the same reduction with no work on the programmer's part.

--- source/reduction.cpp
+++ source/reduction.cpp
@@ -21,6 +21,14 @@
 #include "pipeline.cpp"
 
 
+// List of supported reduction operations.
+// Each supported operation needs a CPU side binary operator and a GPU kernel
+// for each data type. This is because, at least at the moment, reductions are
+// carried out block-wise on the GPU and finished CPU side. A better solution
+// would probably be to use GPU atomics to finalize the reduction but some
+// Vulkan implementations do not support atomics.
+
+
 enum struct redop
 {
   sum,
@@ -29,78 +37,83 @@ enum struct redop
 };
 
 
-template<typename T>
-T redop_sum(T a, T b)
+// CPU side reduction binary operators.
+
+
+template<typename T> T redop_sum(T a, T b) { return a + b;           }
+template<typename T> T redop_min(T a, T b) { return (a < b) ? a : b; }
+template<typename T> T redop_max(T a, T b) { return (a > b) ? a : b; }
+
+
+// Template and specializations coordinating reduction operations between CPU
+// and GPU. Keeping this information together should minimize the necessary code
+// and prevent mistakes. The templating ensures these values match at compile
+// time as long as the template specialization is written correctly. These are
+// intended to provide information about reduction operations in the style of
+// std::numeric_limits.
+
+
+template<redop op, typename dtype>
+class redop_info
 {
-  return a + b;
-}
+  static dtype       reduce(dtype a, dtype b);
+  static std::string shader_file();
+  static dtype       nullval();
+};
 
 
-template<typename T>
-T redop_min(T a, T b)
+template<>
+class redop_info<redop::min, float>
 {
-  return (a < b) ? a : b;
-}
+  static float       reduce(float a, float b) { return redop_min(a, b);     }
+  static std::string shader_file()            { return "reduction_min.spv"; }
+  static float       nullval()                { return FLT_MAX;             }
+};
 
 
-template<typename T>
-T redop_max(T a, T b)
+template<>
+class redop_info<redop::max, float>
 {
-  return (a > b) ? a : b;
-}
+  static float       reduce(float a, float b) { return redop_max(a, b);     }
+  static std::string shader_file()            { return "reduction_max.spv"; }
+  static float       nullval()                { return FLT_MIN;             }
+};
 
 
-template<typename T>
-T reduce(redop op, T a, T b)
+template<>
+class redop_info<redop::sum, float>
 {
-  switch (op)
-  {
-    case redop::sum:
-      return redop_sum(a, b);
-      break;
-    case redop::min:
-      return redop_min(a, b);
-      break;
-    case redop::max:
-      return redop_max(a, b);
-      break;
-  }
-}
+  static float       reduce(float a, float b) { return redop_sum(a, b);     }
+  static std::string shader_file()            { return "reduction_sum.spv"; }
+  static float       nullval()                { return 0.;                  }
+};
 
 
-float reduce_nullval(redop op)
-{
-  switch (op)
-  {
-    case redop::sum:
-      return 0.f;
-      break;
-    case redop::min:
-      return FLT_MAX;
-      break;
-    case redop::max:
-      return FLT_MIN;
-      break;
-  }
-}
+// Workspace for reduction operations.
+// If repeated reduction operations are performed, it wouldn't make sense to
+// keep reallocating supporting structures. These structures are the reduction
+// kernel code itself, the reduced GPU side buffer, and the reduced CPU side
+// buffer. An object of this type is passed to the final reduction routine to
+// enable simple reuse of pre-allocated values.
 
 
+template<redop op, typename dtype>
 struct reduction_workspace
 {
-  redop              optype;
-  compute_pipeline&  op;
+  compute_pipeline shdr;
 
   u32                nblock;
   std::vector<float> h_dst;
   dbuffer<float>     d_dst;
 
-  reduction_workspace(compute_pipeline& op, u32 src_size, u32 blk_size);
+  reduction_workspace(u32 src_size, u32 blk_size);
 };
 
 
-reduction_workspace::reduction_workspace(compute_pipeline& op_, u32 src_size,
-                                         u32 blk_size) :
-op(op_)
+template<redop op, typename dtype>
+reduction_workspace<op, dtype>::reduction_workspace(u32 src_size,
+                                                    u32 blk_size) :
+shdr(SHADER_DIR + redop_info<op, dtype>::shader_file())
 {
   nblock = (src_size + (blk_size - 1)) / blk_size;
   h_dst  = std::vector<float>(nblock);
@@ -109,8 +122,17 @@ op(op_)
 }
 
 
-float float_reduce(dbuffer<float>& arr, dbuffer<u32>& size,
-                   reduction_workspace& worksp)
+// The reduction operator.
+// Performs a reduction using the template specified operation on a GPU
+// allocated array of the template specified data type. Takes a workspace
+// structure to avoid repeated reallocations in certain scenarios. Reduction is
+// performed block-wise on the GPU and transferred to the CPU for final
+// processing.
+
+
+template<redop op, typename dtype>
+dtype reduce(dbuffer<dtype>& arr, dbuffer<u32>& size,
+             reduction_workspace<op, dtype>& worksp)
 {
   worksp.op.dset.update(size,         0);
   worksp.op.dset.update(arr,          1);
@@ -119,10 +141,10 @@ float float_reduce(dbuffer<float>& arr, dbuffer<u32>& size,
 
   memcpy_dtoh(worksp.h_dst.data(), worksp.d_dst);
 
-  float result = reduce_nullval(worksp.optype);
+  float result = redop_info<op, dtype>::nullval();
   for (usize i = 0; i < worksp.nblock; ++i)
   {
-    result = reduce(worksp.optype, result, worksp.h_dst[i]);
+    result = redop_info<op, dtype>::reduce(result, worksp.h_dst[i]);
   }
 
   return result;
